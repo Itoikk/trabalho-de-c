5fdbdb3754b78a19516df52f262251fcb90bc632
Barth <bartholomeu.l@aluno.ifsp.edu.br>
2025-11-13 14:04:59 -0300

    tudududu max verstappen

--- Trabalho de C/Functions.py
+++ Trabalho de C/Functions.py
@@ -13,10 +13,24 @@ def classificar_lados(a, b, c):
         return "As medidas não formam um triângulo válido."
     
 def calcular_area(lado1, lado2, lado3):
-    semiperimetro = (lado1 + lado2 + lado3) / 2
-    area = (semiperimetro * (semiperimetro - lado1) * (semiperimetro - lado2) * (semiperimetro - lado3)) ** 0.5
-    return area
+    if lado1 <= 0 or lado2 <= 0 or lado3 <= 0:
+        return "Os lados devem ser maiores que zero."
+    if (lado1 < lado2 + lado3) and (lado2 < lado1 + lado3) and (lado3 < lado1 + lado2):
+
+        semiperimetro = (lado1 + lado2 + lado3) / 2
+        area = (semiperimetro * (semiperimetro - lado1) * (semiperimetro - lado2) * (semiperimetro - lado3)) ** 0.5
+        return area
+
+    else:
+        return "As medidas não formam um triângulo válido."
 
 def calcular_perimetro(lado1, lado2, lado3):
-    perimetro = lado1 + lado2 + lado3
-    return perimetro
+    if lado1 <= 0 or lado2 <= 0 or lado3 <= 0:
+        return "Os lados devem ser maiores que zero."
+    if (lado1 < lado2 + lado3) and (lado2 < lado1 + lado3) and (lado3 < lado1 + lado2):
+
+        perimetro = lado1 + lado2 + lado3
+        return perimetro
+
+    else:
+        return "As medidas não formam um triângulo válido."
